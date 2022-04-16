f356465d15e25edf7e3c24b42ce31dcdf8d7ef23
Patrick <pdr721@gmail.com>
2022-04-15 23:46:43 -0400

    fixing urls

--- frontpage/urls.py
+++ frontpage/urls.py
@@ -27,5 +27,5 @@ urlpatterns = [
     path('weekreport/', views.weekreport, name='weekreport'), 
     path('welcome/', views.welcome, name='welcome'), 
     path('searchGroups/', views.searchGroups, name='searchGroups'),
-    path('groupView/<int:group_id>', views.groupView)
+    path('groupView/<int:group_id>', views.groupView),
 ]
--- healm/urls.py
+++ healm/urls.py
@@ -25,5 +25,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('frontpage.urls')),
     path('accounts/', include('allauth.urls')),
-    #path('frontpage/', include('frontpage.urls'))  #Lmk if this isn't needed - Latti
 ]
